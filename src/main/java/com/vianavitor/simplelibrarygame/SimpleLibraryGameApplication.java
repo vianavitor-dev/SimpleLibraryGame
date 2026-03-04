f9aeac0a16f34cac120ffdb57f7ec72355ac78d7
vianavitor-dev <vianavitor232@gmail.com>
2026-03-04 15:19:00 -0300

    chore: initial project

---
+++ src/main/java/com/vianavitor/simplelibrarygame/SimpleLibraryGameApplication.java
@@ -0,0 +1,13 @@
+package com.vianavitor.simplelibrarygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLibraryGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vianavitor/simplelibrarygame/SimpleLibraryGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vianavitor.simplelibrarygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
