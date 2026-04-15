5a1995c16f546ada55ca006704fefeada9020234
vianavitor-dev <vianavitor232@gmail.com>
2026-04-15 16:21:33 -0300

    feat: add the claim role to JWT token

--- src/main/java/com/vianavitor/simplelibrarygame/service/auth/TokenService.java
+++ src/main/java/com/vianavitor/simplelibrarygame/service/auth/TokenService.java
@@ -6,11 +6,13 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.vianavitor.simplelibrarygame.model.utils.classes.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
 @Service
 public class TokenService {
@@ -20,10 +22,17 @@ public class TokenService {
     public String generateToken(User user) throws JWTCreationException {
         Algorithm algorithm = Algorithm.HMAC256(secret);
 
+        String role = user.getAuthorities()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new NullPointerException("user role cannot be null"))
+                .toString();
+
         return JWT.create()
                 .withIssuer("simple-library-game")
                 .withSubject(user.getUsername())
                 .withExpiresAt(this.getExpireAt())
+                .withClaim("role", role)
                 .sign(algorithm);
     }
 
