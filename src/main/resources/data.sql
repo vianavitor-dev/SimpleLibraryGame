3c743ba183c7d7e5ab6aefdbe9a81376117b818b
vianavitor-dev <vianavitor232@gmail.com>
2026-04-16 16:47:20 -0300

    fix: bad MYSQL syntax usage
    syntaxes updated and 'INSERT' replaced by 'REPLACE' command

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into user ('active', 'last_login', 'id', 'type', 'name', 'password', 'username')
-values(b'1', null, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
+REPLACE INTO user (active, last_login, id, type, name, password, username)
+VALUES (b'1', NULL, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
 
-insert into genre (id, name) values(1, 'action');
-insert into genre (id, name) values(2, 'sci-fi');
-insert into genre (id, name) values(3, 'thriller');
-insert into genre (id, name) values(4, 'romance');
-insert into genre (id, name) values(5, 'math');
-insert into genre (id, name) values(6, 'art');
-insert into genre (id, name) values(7, 'illustrative');
-insert into genre (id, name) values(8, 'fantasy');
+REPLACE INTO genre (id, name) VALUES(1, 'action');
+REPLACE INTO genre (id, name) VALUES(2, 'sci-fi');
+REPLACE INTO genre (id, name) VALUES(3, 'thriller');
+REPLACE INTO genre (id, name) VALUES(4, 'romance');
+REPLACE INTO genre (id, name) VALUES(5, 'math');
+REPLACE INTO genre (id, name) VALUES(6, 'art');
+REPLACE INTO genre (id, name) VALUES(7, 'illustrative');
+REPLACE INTO genre (id, name) VALUES(8, 'fantasy');
