333638690054f32dac933615424193895ac9313c
vianavitor-dev <vianavitor232@gmail.com>
2026-04-16 16:10:08 -0300

    fix: missing columns into the insert command from data.sql
    due to the missing argument the command was inserting the values into the wrong columns

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into user values(b'1', null, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
+insert into user ('active', 'last_login', 'id', 'type', 'name', 'password', 'username')
+values(b'1', null, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
 
 insert into genre (id, name) values(1, 'action');
 insert into genre (id, name) values(2, 'sci-fi');
