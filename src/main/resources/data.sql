8e644b1b9014ebe127c2bf9a735252954ed97f9b
vianavitor-dev <vianavitor232@gmail.com>
2026-04-16 12:44:42 -0300

    feat: add data.sql to insert data once at the application start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into user values(b'1', null, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
+
+insert into genre (id, name) values(1, 'action');
+insert into genre (id, name) values(2, 'sci-fi');
+insert into genre (id, name) values(3, 'thriller');
+insert into genre (id, name) values(4, 'romance');
+insert into genre (id, name) values(5, 'math');
+insert into genre (id, name) values(6, 'art');
+insert into genre (id, name) values(7, 'illustrative');
+insert into genre (id, name) values(8, 'fantasy');
