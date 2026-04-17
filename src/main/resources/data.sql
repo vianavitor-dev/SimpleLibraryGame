60af438202ffb031a59481f8b476d171e8ddeb30
vianavitor-dev <vianavitor232@gmail.com>
2026-04-17 12:51:47 -0300

    fix: couldn't replace the former data due to table ralationship
    using 'INSERT IGNORE' instead of 'REPLACE' due to FK constrainsts when trying to insert data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-REPLACE INTO user (active, last_login, id, type, name, password, username)
+INSERT IGNORE INTO user (active, last_login, id, type, name, password, username)
 VALUES (b'1', NULL, 1, 'adm', 'admin_1st', '$2a$12$Xfaog7ZIo3RfMpjm9dEk/Or9lqUpM1Tc6vM4zLpW62i0K79LKnYNK', 'admin');
 
-REPLACE INTO genre (id, name) VALUES(1, 'action');
-REPLACE INTO genre (id, name) VALUES(2, 'sci-fi');
-REPLACE INTO genre (id, name) VALUES(3, 'thriller');
-REPLACE INTO genre (id, name) VALUES(4, 'romance');
-REPLACE INTO genre (id, name) VALUES(5, 'math');
-REPLACE INTO genre (id, name) VALUES(6, 'art');
-REPLACE INTO genre (id, name) VALUES(7, 'illustrative');
-REPLACE INTO genre (id, name) VALUES(8, 'fantasy');
+INSERT IGNORE INTO genre (id, name) VALUES(1, 'action');
+INSERT IGNORE INTO genre (id, name) VALUES(2, 'sci-fi');
+INSERT IGNORE INTO genre (id, name) VALUES(3, 'thriller');
+INSERT IGNORE INTO genre (id, name) VALUES(4, 'romance');
+INSERT IGNORE INTO genre (id, name) VALUES(5, 'math');
+INSERT IGNORE INTO genre (id, name) VALUES(6, 'art');
+INSERT IGNORE INTO genre (id, name) VALUES(7, 'illustrative');
+INSERT IGNORE INTO genre (id, name) VALUES(8, 'fantasy');
